1740948214389bd37de207ce0f564f115d12f1a2
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-04 19:04:55 +0530

    second test passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,37 @@
 use proc_macro::TokenStream;
+use proc_macro2::{Ident, Span};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    let code = generate_code(input);
+
+    TokenStream::from(code)
+}
+
+fn generate_code(input: DeriveInput) -> proc_macro2::TokenStream {
+    let item_ident = input.ident;
+    let builder_ident = Ident::new(&format!("{}Builder", item_ident), Span::call_site());
+    quote! {
+        impl #item_ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+    }
 }
--- main.rs
+++ main.rs
@@ -6,4 +6,18 @@
 // To run the code:
 //     $ cargo run
 
-fn main() {}
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    pub executable: String,
+    pub args: Vec<String>,
+    pub env: Vec<String>,
+    pub current_dir: String,
+}
+
+fn main() {
+    let builder = Command::builder();
+
+    let _ = builder;
+}
