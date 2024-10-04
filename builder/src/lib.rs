78dcbdf9e7103f41c0a09a72ad0037c291118b66
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-04 20:50:35 +0530

    refactoring and better error messages

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,26 +1,34 @@
 use proc_macro::TokenStream;
 use proc_macro2::Ident;
-use quote::{format_ident, quote, quote_spanned};
+use quote::{format_ident, quote};
 use syn::{parse_macro_input, Data, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
-    let code = generate_code(input);
+    let code = match generate_code(input) {
+        Ok(code) => code,
+        Err(error) => return error.into_compile_error().into(),
+    };
 
     TokenStream::from(code)
 }
 
-fn generate_code(input: DeriveInput) -> proc_macro2::TokenStream {
+fn generate_code(input: DeriveInput) -> Result<proc_macro2::TokenStream, syn::Error> {
     let item_ident = input.ident;
     let builder_ident = format_ident!("{item_ident}Builder");
 
-    let builder = match generate_builder(&input.data, &item_ident, &builder_ident) {
-        Ok(builder) => builder,
-        Err(error) => return error,
-    };
+    let struct_impl = generate_struct_impl(&item_ident, &builder_ident);
+    let builder = generate_builder_struct(&input.data, &item_ident, &builder_ident)?;
 
+    Ok(quote! {
+        #struct_impl
+        #builder
+    })
+}
+
+fn generate_struct_impl(item_ident: &Ident, builder_ident: &Ident) -> proc_macro2::TokenStream {
     quote! {
         impl #item_ident {
             pub fn builder() -> #builder_ident {
@@ -32,23 +40,22 @@ fn generate_code(input: DeriveInput) -> proc_macro2::TokenStream {
                 }
             }
         }
-
-        #builder
     }
 }
 
-fn generate_builder(
+fn generate_builder_struct(
     data: &Data,
     item_ident: &Ident,
     builder_ident: &Ident,
-) -> Result<proc_macro2::TokenStream, proc_macro2::TokenStream> {
+) -> Result<proc_macro2::TokenStream, syn::Error> {
     match data {
         Data::Struct(data_struct) => {
             let is_tuple_struct = data_struct.fields.iter().any(|f| f.ident.is_none());
             if is_tuple_struct {
-                return Err(quote_spanned! { item_ident.span() =>
-                    compile_error!("#[derive(Builder)] does not work for a tuple struct");
-                });
+                return Err(syn::Error::new(
+                    item_ident.span(),
+                    "#[derive(Builder)] does not work for a tuple struct",
+                ));
             }
             let mut fields = Vec::with_capacity(data_struct.fields.len());
             for field in &data_struct.fields {
@@ -64,11 +71,13 @@ fn generate_builder(
                 }
             })
         }
-        Data::Enum(_) => Err(quote_spanned! { item_ident.span() =>
-            compile_error!("#[derive(Builder)] does not work for an enum");
-        }),
-        Data::Union(_) => Err(quote_spanned! { item_ident.span() =>
-            compile_error!("#[derive(Builder)] does not work for a union");
-        }),
+        Data::Enum(_) => Err(syn::Error::new(
+            item_ident.span(),
+            "#[derive(Builder)] does not work for an enum",
+        )),
+        Data::Union(_) => Err(syn::Error::new(
+            item_ident.span(),
+            "#[derive(Builder)] does not work for a union",
+        )),
     }
 }
