c43175dcfed85c6e5e257033eac55f59dedae40b
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-04 20:20:26 +0530

    remove hardcoded field names and show proper error messages

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
-use proc_macro2::{Ident, Span};
-use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use proc_macro2::Ident;
+use quote::{format_ident, quote, quote_spanned};
+use syn::{parse_macro_input, Data, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -14,7 +14,13 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
 fn generate_code(input: DeriveInput) -> proc_macro2::TokenStream {
     let item_ident = input.ident;
-    let builder_ident = Ident::new(&format!("{}Builder", item_ident), Span::call_site());
+    let builder_ident = format_ident!("{item_ident}Builder");
+
+    let builder = match generate_builder(&input.data, &item_ident, &builder_ident) {
+        Ok(builder) => builder,
+        Err(error) => return error,
+    };
+
     quote! {
         impl #item_ident {
             pub fn builder() -> #builder_ident {
@@ -27,11 +33,42 @@ fn generate_code(input: DeriveInput) -> proc_macro2::TokenStream {
             }
         }
 
-        pub struct #builder_ident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
+        #builder
+    }
+}
+
+fn generate_builder(
+    data: &Data,
+    item_ident: &Ident,
+    builder_ident: &Ident,
+) -> Result<proc_macro2::TokenStream, proc_macro2::TokenStream> {
+    match data {
+        Data::Struct(data_struct) => {
+            let is_tuple_struct = data_struct.fields.iter().any(|f| f.ident.is_none());
+            if is_tuple_struct {
+                return Err(quote_spanned! { item_ident.span() =>
+                    compile_error!("#[derive(Builder)] does not work for a tuple struct");
+                });
+            }
+            let mut fields = Vec::with_capacity(data_struct.fields.len());
+            for field in &data_struct.fields {
+                let name = field.ident.as_ref().unwrap();
+                let ty = &field.ty;
+                fields.push(quote! {
+                    #name: Option<#ty>,
+                });
+            }
+            Ok(quote! {
+                pub struct #builder_ident {
+                    #(#fields)*
+                }
+            })
         }
+        Data::Enum(_) => Err(quote_spanned! { item_ident.span() =>
+            compile_error!("#[derive(Builder)] does not work for an enum");
+        }),
+        Data::Union(_) => Err(quote_spanned! { item_ident.span() =>
+            compile_error!("#[derive(Builder)] does not work for a union");
+        }),
     }
 }
