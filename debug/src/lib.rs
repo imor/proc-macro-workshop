8071550517d66c75e561dc64a2f91bf5892c1669
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-15 11:01:55 +0530

    second debug test passing

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,10 +1,17 @@
-use proc_macro::TokenStream;
-use syn::{parse2, DeriveInput, Result};
+use quote::quote;
+use syn::{parse2, Data, DeriveInput, Result};
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    match parse(input.into()) {
-        Ok(_) => TokenStream::new(),
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = match parse(input.into()) {
+        Ok(input) => input,
+        Err(e) => {
+            return e.into_compile_error().into();
+        }
+    };
+
+    match generate_code(input) {
+        Ok(stream) => stream.into(),
         Err(e) => e.into_compile_error().into(),
     }
 }
@@ -12,4 +19,30 @@ pub fn derive(input: TokenStream) -> TokenStream {
 fn parse(input: proc_macro2::TokenStream) -> Result<DeriveInput> {
     let derive_input = parse2(input)?;
     Ok(derive_input)
+}
+
+fn generate_code(input: DeriveInput) -> Result<proc_macro2::TokenStream> {
+    let name = input.ident;
+    let Data::Struct(data_struct) = input.data else {
+        return Err(syn::Error::new(
+            name.span(),
+            "only structs supported currently",
+        ));
+    };
+
+    let fields = data_struct.fields.iter().map(|f| {
+        let field_name = f.ident.clone().unwrap();
+        quote! { .field(stringify!(#field_name), &self.#field_name) }
+    });
+
+    let code = quote! {
+        impl std::fmt::Debug for #name {
+            fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                fmt.debug_struct(stringify!(#name))
+                    #(#fields)*
+                   .finish()
+            }
+        }
+    };
+    Ok(code)
 }
