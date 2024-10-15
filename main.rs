0bdffb24703ca8445871d9b11e997d1ccdd39c6d
Raminder Singh <romi_ssk@yahoo.co.in>
2024-10-15 10:36:39 +0530

    fix warning

--- main.rs
+++ main.rs
@@ -10,8 +10,8 @@ use derive_debug::CustomDebug;
 
 #[derive(CustomDebug)]
 pub struct Field {
-    name: &'static str,
-    bitmask: u16,
+    pub name: &'static str,
+    pub bitmask: u16,
 }
 
 fn main() {}
